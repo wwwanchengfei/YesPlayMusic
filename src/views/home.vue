d35d06413d8461214bff1bbd266125eb3697574b
wan.chengfei <dev.wanchengfei@gmail.com>
2023-08-17 17:06:21 +0800

    去除 Apple Music

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,17 @@
 <template>
   <div v-show="show" class="home">
-    <div
+    <!-- <div
       v-if="settings.showPlaylistsByAppleMusic !== false"
       class="index-row first-row"
     >
-      <!-- <div class="title"> by Apple Music </div>
+      <div class="title"> by Apple Music </div>
       <CoverRow
         :type="'playlist'"
         :items="byAppleMusic"
         sub-text="appleMusic"
         :image-size="1024"
-      /> -->
-    </div>
+      />
+    </div> -->
     <div class="index-row">
       <div class="title">
         {{ $t('home.recommendPlaylist') }}
